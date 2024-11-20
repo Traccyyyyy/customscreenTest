c9e0d0f986249d7cbf3d7a50427b6cead5bd65cc
Traccyyyyy <139620184+Traccyyyyy@users.noreply.github.com>
2024-11-21 03:04:56 +1100

    try this.NoahFace

--- src/lit-components/customize-greeting.js
+++ src/lit-components/customize-greeting.js
@@ -76,7 +76,7 @@ export class customizeGreeting extends LitElement {
     const { firstname, lastname, org, site, temperature, elevated, eventtype: type, eventtime, eventdetails: details } = this.noahFaceData;
 
     return html`
-      ${this.showGreeting ? html`<h2>${this.greeting}, ${window.NoahFace.firstname|| 'Guest'}${this.showFullName && lastname ? ` ${lastname}` : ''}!</h2>` : ''}
+      ${this.showGreeting ? html`<h2>${this.greeting}, ${this.NoahFace.firstname|| 'Guest'}${this.showFullName && lastname ? ` ${lastname}` : ''}!</h2>` : ''}
       ${this.showFullName && firstname ? html`<p>First Name: ${firstname}</p>
         <p>Last Name: ${lastname}</p>`:''}
       ${this.showOrg && org ? html`<p>Organization: ${org}</p>` : ''}
